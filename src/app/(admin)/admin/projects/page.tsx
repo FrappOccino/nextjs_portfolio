328ba72513ff74cb9a0fd5101134cbb8c9f9994f
wong <wongjeffreyg@gmail.com>
2026-08-18 19:58:51 +0800

    implement delete in projects

--- src/app/(admin)/admin/projects/page.tsx
+++ src/app/(admin)/admin/projects/page.tsx
@@ -3,28 +3,55 @@
 import { useEffect, useMemo, useState } from "react";
 import { AllCommunityModule, ColDef , RowHeightParams } from "ag-grid-community";
 import { AgGridProvider, AgGridReact } from "ag-grid-react";
-import { getProjects   } from "@/features/projects/service";
+import { getProjects  , deleteProject , deleteThumbNail} from "@/features/projects/service";
 import Link from 'next/link';
 import { Icon } from "@iconify/react";
 import Image from 'next/image'
 import { ICellRendererParams } from "ag-grid-community";
 
 
-const ThumbnailImage = (props : ICellRendererParams) => {
+const ThumbnailImage = (props : any) => {
   return (
     <div className="flex items-center justify-center h-full">
-      <Image
+      {/* <Image
         src={props.data.thumb_nail}
         width={100}
         height={100}
         alt={props.data.title ?? "Project thumbnail"}
         className="w-[100px] h-[100px] object-cover"
+      /> */}
+      <img
+          src={props.data.thumb_nail}
+          alt="Thumbnail"
       />
     </div>
   );
 };
 
 export default function Page() {
+
+  const deleteProjectAction = async (id : any , url : any) => {
+      console.log("deleteProjectAction ID:" , id , url);
+      try {
+        await deleteProject(id);
+        await deleteThumbNail(url);
+  
+        // Remove the deleted row from the UI
+        setRowData((currentRows) =>
+          currentRows.filter((row) => row.id !== id)
+        );
+      } catch (error) {
+        console.error("Failed to delete project:", error);
+      }
+    };
+  
+  
+  const deleteButtonComponent = (props : ICellRendererParams) => {
+    return (
+              <button onClick={() => deleteProjectAction(props.data.id , props.data.thumb_nail)} className="btn btn-xs btn-error">delete</button>
+      );
+  };
+
   const [rowData, setRowData] = useState<any[]>([]);
 
 
@@ -54,6 +81,8 @@ export default function Page() {
 
     {
       field: "action",
+      headerName: "action",
+      cellRenderer: deleteButtonComponent
     },
   ];
 
--- src/features/projects/service.ts
+++ src/features/projects/service.ts
@@ -3,6 +3,7 @@
 import { projectsRepository } from "@/features/projects/repository";
 import { writeFile } from "fs/promises"
 import path from "path"
+import { del } from '@vercel/blob';
 
 export async function getProjects() {
     return await projectsRepository.findAll();
@@ -12,4 +13,20 @@ export async function getProjects() {
 
 export async function createProject(data : any ) {
     return await projectsRepository.create(data);
+}
+
+export async function deleteProject(data: any) {
+    await projectsRepository.delete(data);
+
+    return {
+        success: true,
+    };
+}
+
+export async function deleteThumbNail(urlToDelete: any) {
+    await del(urlToDelete);
+
+    return {
+        success: true,
+    };
 }
